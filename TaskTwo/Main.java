fcdf087067f4b1e6525c2238cdff5bcbf7927f76
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-11-15 11:27:52 +0200

    Update Main.java
    Finished task two

--- TaskTwo/Main.java
+++ TaskTwo/Main.java
@@ -1,5 +1,30 @@
+import java.util.*; 
 public class Main{
 	public static void main(String[] args) {
-
+        System.out.println("Please enter the number of students");
+        Scanner input = new Scanner(System.in);
+        int numOfStudents =  input.nextInt();
+        
+        int highestScore = 0;
+        String name = "Null";
+        while(numOfStudents > 0){
+            System.out.print("Please enter the student's name : ");
+            input.nextLine();
+            String tempName = input.nextLine();
+            System.out.println();
+            
+            System.out.print("Please enter the student's score : ");
+            int tempScore = input.nextInt();
+            System.out.println();
+            
+            if(tempScore > highestScore){
+                highestScore = tempScore;
+                name = tempName;
+            }
+            
+            numOfStudents--;
+        }
+        System.out.println("The student with the highest score is "+name+" with a score of "+highestScore);;
+        
 	}
 }
