67bb1418ad65d8048b610b300756c10231471f0c
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-11-19 14:06:59 +0200

    Update Main.java
    Updated objTwo to meet the new standard of Person class

--- TaskFour/Main.java
+++ TaskFour/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Address(String streetName, String section, String suburb, int postalCode)
-		Address objOne = new Address( "14 Magic Street", "Circus", "Disney",526);
+		//Address objOne = new Address( "14 Magic Street", "Circus", "Disney",526);
 		
-		// Person( String name, String lastName, int age, Address obj)
-		Person objTwo =  new Person( "John", "Wick", 55, objOne);
+		// Person( String name, String lastName, int age, String streetName, String section, String suburb, int postalCode)
+		Person objTwo =  new Person( "John", "Wick", 55,  "14 Magic Street", "Circus", "Disney",526);
 		objTwo.printPerson();
 	}
 
