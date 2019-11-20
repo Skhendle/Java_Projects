a96052be45aa6b0cd6b3e54e37617256b8a3d18c
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-11-20 15:57:41 +0200

    Create Main.java
    The main file for this task

---
+++ TaskFive/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//	public Students(String studentID, String major, String department, int semester, String name, String lastName, int age, 
+		//String streetName, String section, String suburb, int postalCode)
+		
+		Student objTwo =  new Student("664726", "Computer Science", "Science", 8 ,"John", "Wick",
+				55,  "14 Magic Street", "Circus", "Disney",526);
+		objTwo.printStudent();
+	}
+
+}
