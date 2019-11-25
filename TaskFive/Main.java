8ecab578643240e42b08d0b5eae5d39bcb58b755
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-11-25 08:59:53 +0200

    Update Main.java
    Added an object for the main class

--- TaskFive/Main.java
+++ TaskFive/Main.java
@@ -10,6 +10,15 @@ public class Main {
 		Student objTwo =  new Student("664726", "Computer Science", "Science", 8 ,"John", "Wick",
 				55,  "14 Magic Street", "Circus", "Disney",526);
 		objTwo.printStudent();
+		
+		//Instructor(String instructorID, String degree, int classesTeaching, String major, String department, int yearsWorking, 
+			//	double salary, String name, String lastName, int age, String streetName, String section, String suburb,int postalCode)
+		System.out.println();
+		System.out.println();
+		Instructor obj = new Instructor("98564", "Computer Science", 2," Machine Learning", 
+				"Computational and Advanced mathematics", 10 ,65203.72, 
+				"jane", "Doe",37,"14 Magic Street", "Circus", "Disney",526);
+		obj.printInstructor();
 	}
 
 }
