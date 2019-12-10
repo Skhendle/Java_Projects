adf885eac2a24c02a109aedfe0526758771dfc95
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-12-10 11:10:03 +0200

    Update Main.java
    Added content of the main fail for  Exception handling

--- TaskSix/Main.java
+++ TaskSix/Main.java
@@ -1,1 +1,23 @@
+public class Main {
 
+	public void division(double a, double b) throws IllegalArgumentException{
+		if (b != 0) {
+			double div = a/b;
+			System.out.println(" a = "+a+", b = "+b+", a/b = "+div);
+		}else{
+			System.out.println();
+			throw new IllegalArgumentException("Cannot divide "+a+" by zero.");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double numOne = 10.0, numTwo = 15.0;
+		Main obj = new Main();
+		obj.division(numOne, numTwo);
+		obj.division(numOne, 0.0);
+		
+		
+
+	}
+
+}
