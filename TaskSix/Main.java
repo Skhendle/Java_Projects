e3502103e6a1630e3b1d1d9e85fdd1641b4d4a3e
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-12-10 11:32:10 +0200

    Update Main.java
    Added the second part of tasksix, commented out the exception for question since it does not allow us to continue with the program

--- TaskSix/Main.java
+++ TaskSix/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public void division(double a, double b) throws IllegalArgumentException{
@@ -7,16 +9,36 @@ public class Main {
 		}else{
 			System.out.println();
 			throw new IllegalArgumentException("Cannot divide "+a+" by zero.");
+
 		}
+		
+	}
+	
+	public void arrayGenerate(int a) {
+		
+		int[] array = new int[50];
+		for(int i = 0; i < 50; i++) {
+			array[i]= (int)(Math.random()*((500-i)+1))+i;
+		}
+		
+		System.out.println( "The value at index="+a+" is "+array[a]);
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// This is the solution to TaskSix question one
 		double numOne = 10.0, numTwo = 15.0;
 		Main obj = new Main();
 		obj.division(numOne, numTwo);
-		obj.division(numOne, 0.0);
+//		obj.division(numOne, 0.0);
 		
-		
+		// This is the solution to TaskSix question two
+		try {
+			obj.arrayGenerate(10);
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new ArrayIndexOutOfBoundsException("Out of bounds");
+		}
 
 	}
 
